35e9c6c71bb5ebebe01badab3ac27ebfcf994926
Thais Moraes <murici.thais@gmail.com>
2019-04-16 16:41:03 -0300

    Add initial Redux/Redux Saga/Reactotron structure

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './ducks';
+import rootSaga from './sagas';
+
+const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    console.tron.createEnhancer(),
+  ),
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
