5562a5ff49524faa1bbe3e159370c1d6afb68ff6
Thais Moraes <murici.thais@gmail.com>
2019-04-20 11:20:39 -0300

    Add initial configuration for Track Player

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,8 +1,15 @@
 import { all, takeLatest } from 'redux-saga/effects';
 
 import { PodcastsTypes } from '~/store/ducks/podcasts';
+import { PlayerTypes } from '~/store/ducks/player';
+
+import { init, setPodcast } from './player';
 import { load } from './podcasts';
 
 export default function* rootSaga() {
-  return yield all([takeLatest(PodcastsTypes.LOAD_REQUEST, load)]);
+  return yield all([
+    init(),
+    takeLatest(PodcastsTypes.LOAD_REQUEST, load),
+    takeLatest(PlayerTypes.SET_PODCAST_REQUEST, setPodcast),
+  ]);
 }
